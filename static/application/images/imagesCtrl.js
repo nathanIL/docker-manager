46c4cea0b4051064bfa0d9e039ba6d8e579efc7c
Nathan Abu <nathanabu@gmail.com>
2015-08-23 00:54:56 +0300

    Images section cont.

--- static/application/images/imagesCtrl.js
+++ static/application/images/imagesCtrl.js
@@ -1,5 +1,28 @@
 angular.module('manager.components',[]).
-    controller('imagesCtrl',['$scope',function($scope) {
+  controller('imagesCtrl',['$scope','Image', function($scope,Image) {
+    $scope.imagesGrid = {
+                enableSorting: true,
+                enableHiding: false,
+                columnDefs: [
+                  { field: 'name' },
+                  { field: 'id' },
+                  { field: 'created' }
+                ],
+//                onRegisterApi: function( gridApi ) {
+//                  $scope.grid1Api = gridApi;
+//                }
+              };
+     Image.query( function(d) {
+        $scope.imagesGrid.data = [];
+        angular.forEach(d, function(v,k) {
+                console.log(v);
+                $scope.imagesGrid.data.push( { name: v.RepoTags.join(','),
+                                               id: v.Id.substring(0,12),
+                                               created: 'april' } );
+
+        } );
+        //[{ name: 'Job', id: 'xxxxx', created: 'april'}];
 
 
-    } ]);
+     });
+  } ]);
