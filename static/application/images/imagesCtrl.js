b5a6ba72e25631db40a3de2c1a738c29c44b225f
Nathan Abu <nathanabu@gmail.com>
2015-08-24 01:30:47 +0300

    Images section dev

--- static/application/images/imagesCtrl.js
+++ static/application/images/imagesCtrl.js
@@ -3,26 +3,28 @@ angular.module('manager.components',[]).
     $scope.imagesGrid = {
                 enableSorting: true,
                 enableHiding: false,
-                columnDefs: [
-                  { field: 'name' },
-                  { field: 'id' },
-                  { field: 'created' }
-                ],
-//                onRegisterApi: function( gridApi ) {
-//                  $scope.grid1Api = gridApi;
-//                }
-              };
+                columnDefs: [ { field: 'name' },
+                              { field: 'tag' },
+                              { field: 'id' },
+                              { field: 'created' },
+                              { field: 'action' }]
+
+                };
      Image.query( function(d) {
         $scope.imagesGrid.data = [];
         angular.forEach(d, function(v,k) {
-                console.log(v);
-                $scope.imagesGrid.data.push( { name: v.RepoTags.join(','),
-                                               id: v.Id.substring(0,12),
-                                               created: 'april' } );
+                var date = new Date(0);
+                date.setUTCSeconds(v['Created']);
+                var formatted_date = date.getDate() + "/" + ( date.getMonth() + 1) + "/" + date.getFullYear();
 
+                angular.forEach(v.RepoTags, function(vs,ks) {
+                    var name = vs.split(':');
+                    $scope.imagesGrid.data.push( { name: name[0],
+                                                   tag: name[1],
+                                                   id: v.Id.substring(0,12),
+                                                   created: formatted_date } );
+                });
         } );
-        //[{ name: 'Job', id: 'xxxxx', created: 'april'}];
-
 
      });
   } ]);
