91e5b1972fd7a2dadbcd6d0d8391a18d7c137c0c
Nathan Abu <nathanabu@gmail.com>
2015-09-05 15:50:10 +0300

    Fix popovers and add containers table popover

--- static/application/dashboard/dashboardCtrl.js
+++ static/application/dashboard/dashboardCtrl.js
@@ -22,15 +22,7 @@ angular.module('manager.dashboard',[])
                       labels: ['Images'],
                       parseTime: false });
     });
-
-    // TODO: Can we move these strings to a template?
-    Popover.show({ element: '#ImagesCreationTrendChartPopOver',
-                    title: 'Images creation trend graph',
-                    content: 'This graph shows the images creation trend. The dates reflect the image ' +
-                             'creation date and not the pull date.<br/><br/>' +
-                             '<b>Image:</b> A Docker image is a read-only template. For example, an image could contain an Ubuntu operating system with Apache and your web application installed. Images are used to create Docker containers. Docker provides a simple way to build new images or update existing images, or you can download Docker images that other people have already created. Docker images are the build component of Docker.'});
-
-     /* End of Images creation trend chart population */
+    /* End of Images creation trend chart population */
 
 
     /* Containers chart and table */
@@ -68,12 +60,31 @@ angular.module('manager.dashboard',[])
              updateContainers();
     };
     updateContainers();
-    // TODO: Can we move these strings to a template?
+    /* End of Containers chart and table */
+
+    /* Popovers */
+    Popover.show({ element: '#ImagesCreationTrendChartPopOver',
+                    title: 'Images creation trend graph',
+                    content: 'This graph shows the images creation trend. The dates reflect the image ' +
+                             'creation date and not the pull date.<br/><br/>' +
+                             '<b>Image:</b> A Docker image is a read-only template. For example, an image could contain an Ubuntu operating system with Apache and your web application installed. Images are used to create Docker containers. Docker provides a simple way to build new images or update existing images, or you can download Docker images that other people have already created. Docker images are the build component of Docker.'});
+
     Popover.show({ element: '#ContainersChartPopOver',
                    title: 'Containers status graph',
                    content: 'This graph shows the status of the current active containers.<br/></br>' +
                             '<b>Containers:</b> Docker containers are similar to a directory. A Docker container holds everything that is needed for an application to run. Each container is created from a Docker image. Docker containers can be run, started, stopped, moved, and deleted. Each container is an isolated and secure application platform. Docker containers are the run component of Docker.' });
 
-   /* End of Containers chart and table */
+    Popover.show({ element: '#ContainersListPopOver',
+                    title: 'Containers list table',
+                    content: "This table lists the currently available containers and the available actions depending on the container's status. <br/><br/>" +
+                             '<b>Actions</b>:<br/>' +
+                             '<ul>' +
+                             '<li><u>Stop</u>: Stop a running container by sending SIGTERM and then SIGKILL after a grace period</li>' +
+                             '<li><u>Kill</u>: Kill a running container using SIGKILL (Specific signals can be used on the <i>Containers</i> section)</li>' +
+                             '<li><u>Pause</u>: Pause all processes within a container</li>' +
+                             '<li><u>Unpause</u>: Unpause all processes within a paused container</li>' +
+                             '<li><u>Remove</u>: Remove the container and all of its volumes</li>' +
+                             '</ul>'
+    });
 
-  } ]);
+  }]);
